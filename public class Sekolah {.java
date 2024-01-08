f9e5c129ce0930c412555684a45cedf495bbb7b1
FlyLeap12 <141807503+FlyLeap12@users.noreply.github.com>
2024-01-08 20:05:52 +0700

    Add files via upload

---
+++ public class Sekolah {.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sekolah {
+    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Saya Bersekolah di "+nama);
+
+    }
+    public void jumlahsiswa (int rombel, int jumlahsiswa){
+        int total=rombel*jumlahsiswa;
+        System.out.println("Jumlah Siswa: "+total);
+    }
+}
+// percobaan git
